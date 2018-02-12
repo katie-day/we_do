fe2826234931f14d185770bf94f4c3597b6d7b91
Katie Day <katie@springload.co.nz>
2018-02-13 12:59:16 +1300

    changing to stateless

--- src/js/components/common/header/Header.component.js
+++ src/js/components/common/header/Header.component.js
@@ -1,34 +1,30 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
+// import PropTypes from 'prop-types';
 
-class Header extends Component {
-    super(props) {}
-    render() {
-        const { guest } = this.props;
-        // const id = guest.id ? guest.id : '';
-        return (
-            <div className="header" id="header">
-                <div className="gutters">
-                    <div className="grid-flex">
-                        <div className="header__logo-group">
-                            <p className="header__logo header__menu-item">
-                                <a href="https://daleandkatie.co.nz/">K / D</a>
-                            </p>
-                        </div>
+const Header = () => {
+    // const id = guest.id ? guest.id : '';
+    return (
+        <div className="header" id="header">
+            <div className="gutters">
+                <div className="grid-flex">
+                    <div className="header__logo-group">
+                        <p className="header__logo header__menu-item">
+                            <a href="https://daleandkatie.co.nz/">K / D</a>
+                        </p>
                     </div>
                 </div>
             </div>
-        );
-    }
-}
-
-Header.propTypes = {
-    guest: PropTypes.object,
+        </div>
+    );
 };
 
-Header.defaultProps = {
-    guest: {},
-};
+// Header.propTypes = {
+//     guest: PropTypes.object,
+// };
+
+// Header.defaultProps = {
+//     guest: {},
+// };
 
 export default Header;
 
